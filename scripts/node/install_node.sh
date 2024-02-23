efec2087b3497bc239440c5d8ce33e614c985313
BF-202307041359\Administrator <2417035596@qq.com>
2024-02-23 17:01:01 +0800

    111

--- scripts/node/install_node.sh
+++ scripts/node/install_node.sh
@@ -5,11 +5,11 @@ set -e
 cd /home/buercorp
 
 wget --no-check-certificate https://registry.npmmirror.com/-/binary/node/latest-v18.x/node-v18.12.1-linux-x64.tar.gz
-sudo mv node-v18.12.1-linux-x64.tar.gz apps/node-v18.12.1.tar.gz
-cd apps
-tar xvf node-v18.12.1.tar.gz
+tar xvf node-v18.12.1-linux-x64.tar.gz
+sudo mv node-v18.12.1-linux-x64 apps/node-v18.12.1
 sudo rm node-v18.12.1.tar.gz
 
+cd apps
 ln -sf node-v18.12.1 node
 # echo "export PATH=$PATH:/home/lu-jiewei/apps/node/bin" >> /etc/profile
 # source /etc/profile
