aea657a534c7657828faf1e652f0eda0cc77ea22
BF-202307041359\Administrator <2417035596@qq.com>
2024-02-23 16:51:31 +0800

    111

--- scripts/node/install_node.sh
+++ scripts/node/install_node.sh
@@ -8,7 +8,7 @@ wget --no-check-certificate https://registry.npmmirror.com/-/binary/node/latest-
 sudo mv node-v18.12.1-linux-x64.tar.gz apps/node-v18.12.1.tar.gz
 cd apps
 tar xvf node-v18.12.1.tar.gz
-rm node-v18.12.1.tar.gz
+sudo rm node-v18.12.1.tar.gz
 
 ln -sf node-v18.12.1 node
 # echo "export PATH=$PATH:/home/lu-jiewei/apps/node/bin" >> /etc/profile
