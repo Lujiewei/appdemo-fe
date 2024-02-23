3c44cf375ab60d11794785cc7c1474cc05654d81
BF-202307041359\Administrator <2417035596@qq.com>
2024-02-23 16:11:13 +0800

    更新了安装nginx和node的脚本

--- scripts/node/install_node.sh
+++ scripts/node/install_node.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-cdcd /home/buercorp
+cd /home/buercorp
 
 wget --no-check-certificate https://registry.npmmirror.com/-/binary/node/latest-v18.x/node-v18.12.1-linux-x64.tar.gz
-mv node-v18.12.1-linux-x64.tar.gz /home/buercorp/apps/node-v18.12.1-linux-x64.tar.gz
-tar xvf node-v18.12.1-linux-x64.tar.gz
-rm /home/buercorp/apps/node-v18.12.1-linux-x64.tar.gz
+mv node-v18.12.1-linux-x64.tar.gz apps/node-v18.12.1.tar.gz
+tar xvf node-v18.12.1.tar.gz
+rm node-v18.12.1.tar.gz
 
 ln -sf node-v18.12.1 node
 # echo "export PATH=$PATH:/home/lu-jiewei/apps/node/bin" >> /etc/profile
