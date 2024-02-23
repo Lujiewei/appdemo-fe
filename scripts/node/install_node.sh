41431eb5270143c6f331fe6c9c6b2212a820ca37
BF-202307041359\Administrator <2417035596@qq.com>
2024-02-23 17:04:04 +0800

    111

--- scripts/node/install_node.sh
+++ scripts/node/install_node.sh
@@ -7,7 +7,7 @@ cd /home/buercorp
 wget --no-check-certificate https://registry.npmmirror.com/-/binary/node/latest-v18.x/node-v18.12.1-linux-x64.tar.gz
 tar xvf node-v18.12.1-linux-x64.tar.gz
 sudo mv node-v18.12.1-linux-x64 apps/node-v18.12.1
-sudo rm node-v18.12.1.tar.gz
+sudo rm node-v18.12.1-linux-x64.tar.gz
 
 cd apps
 ln -sf node-v18.12.1 node
